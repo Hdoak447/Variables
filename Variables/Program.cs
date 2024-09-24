2956c4ae46e82dcbce52b6ec7cb9c15f6883c316
harry <harry@LAPTOP-30Q0ORI0>
2024-09-24 14:18:35 +0100

    Add project files.

---
+++ Variables/Program.cs
@@ -0,0 +1,7 @@
+﻿int firstScore = 55;
+int secondScore = 4;
+secondScore = firstScore;
+firstScore = -1;
+Console.WriteLine("The content of firstScore is " + firstScore);
+Console.WriteLine("The content of secondScore is " + secondScore);
+Console.ReadKey();  
